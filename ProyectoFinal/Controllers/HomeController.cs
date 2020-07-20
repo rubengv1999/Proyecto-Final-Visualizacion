792c3f1095d8d593f579fe62f4fa4e7b21a04e36
Rubén González Villanueva <rubengv1999@hotmail.com>
2020-07-20 07:34:35 -0600

    Explicacion .NET

--- ProyectoFinal/Controllers/HomeController.cs
+++ ProyectoFinal/Controllers/HomeController.cs
@@ -38,8 +38,12 @@ namespace ProyectoFinal.Controllers
                 while (reader.Read())
                     paisesViewModel.Add(new PaisViewModel(reader.GetString(0), reader.GetString(1), reader.GetString(2), reader.GetString(3), Convert.ToDouble(reader.GetValue(4)), Convert.ToDouble(reader.GetValue(5)), Convert.ToDouble(reader.GetValue(6)), Convert.ToDouble(reader.GetValue(7)), Convert.ToDouble(reader.GetValue(8)), Convert.ToDouble(reader.GetValue(9)), Convert.ToDouble(reader.GetValue(10)), Convert.ToDouble(reader.GetValue(11)), Convert.ToDouble(reader.GetValue(12)), Convert.ToDouble(reader.GetValue(13)), Convert.ToDouble(reader.GetValue(14)), Convert.ToDouble(reader.GetValue(15)), Convert.ToDouble(reader.GetValue(16)), Convert.ToDouble(reader.GetValue(17)), reader.GetString(18), Convert.ToDouble(reader.GetValue(19)), Convert.ToDouble(reader.GetValue(20)), Convert.ToDouble(reader.GetValue(21)), Convert.ToDouble(reader.GetValue(22)), Convert.ToDouble(reader.GetValue(23)), Convert.ToDouble(reader.GetValue(24)), Convert.ToDouble(reader.GetValue(25)), Convert.ToDouble(reader.GetValue(26)), Convert.ToDouble(reader.GetValue(27)), Convert.ToDouble(reader.GetValue(28)), Convert.ToDouble(reader.GetValue(29)), Convert.ToDouble(reader.GetValue(30)), Convert.ToDouble(reader.GetValue(31)), Convert.ToDouble(reader.GetValue(32)), Convert.ToDouble(reader.GetValue(33))));
             } while (reader.NextResult());
+
             return View();
         }
+
+        [HttpGet]
+        public IEnumerable<string> GetDates() => paisesViewModel.Select(x => x.Date).Distinct().ToList();
 
         [HttpGet]
         public IEnumerable<PaisViewModel> GetDataByDate(string Date) => paisesViewModel.Where(x => x.Date == Date);
