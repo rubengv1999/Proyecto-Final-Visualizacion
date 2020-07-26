3fcd5b2174a6b441b172a82808ceadd724c75e39
Rubén González Villanueva <rubengv1999@hotmail.com>
2020-07-26 15:39:18 -0600

    Home

--- ProyectoFinal/Controllers/HomeController.cs
+++ ProyectoFinal/Controllers/HomeController.cs
@@ -36,9 +36,10 @@ namespace ProyectoFinal.Controllers
             {
                 reader.Read();
                 while (reader.Read())
-                    paisesViewModel.Add(new PaisViewModel(reader.GetString(0), reader.GetString(1), reader.GetString(2), reader.GetString(3), Convert.ToDouble(reader.GetValue(4)), Convert.ToDouble(reader.GetValue(5)), Convert.ToDouble(reader.GetValue(6)), Convert.ToDouble(reader.GetValue(7)), Convert.ToDouble(reader.GetValue(8)), Convert.ToDouble(reader.GetValue(9)), Convert.ToDouble(reader.GetValue(10)), Convert.ToDouble(reader.GetValue(11)), Convert.ToDouble(reader.GetValue(12)), Convert.ToDouble(reader.GetValue(13)), Convert.ToDouble(reader.GetValue(14)), Convert.ToDouble(reader.GetValue(15)), Convert.ToDouble(reader.GetValue(16)), Convert.ToDouble(reader.GetValue(17)), reader.GetString(18), Convert.ToDouble(reader.GetValue(19)), Convert.ToDouble(reader.GetValue(20)), Convert.ToDouble(reader.GetValue(21)), Convert.ToDouble(reader.GetValue(22)), Convert.ToDouble(reader.GetValue(23)), Convert.ToDouble(reader.GetValue(24)), Convert.ToDouble(reader.GetValue(25)), Convert.ToDouble(reader.GetValue(26)), Convert.ToDouble(reader.GetValue(27)), Convert.ToDouble(reader.GetValue(28)), Convert.ToDouble(reader.GetValue(29)), Convert.ToDouble(reader.GetValue(30)), Convert.ToDouble(reader.GetValue(31)), Convert.ToDouble(reader.GetValue(32)), Convert.ToDouble(reader.GetValue(33))));
+                    if(!reader.GetString(2).Equals("World"))
+                        paisesViewModel.Add(new PaisViewModel(reader.GetString(0), reader.GetString(1), reader.GetString(2), reader.GetString(3), Convert.ToDouble(reader.GetValue(4)), Convert.ToDouble(reader.GetValue(5)), Convert.ToDouble(reader.GetValue(6)), Convert.ToDouble(reader.GetValue(7)), Convert.ToDouble(reader.GetValue(8)), Convert.ToDouble(reader.GetValue(9)), Convert.ToDouble(reader.GetValue(10)), Convert.ToDouble(reader.GetValue(11)), Convert.ToDouble(reader.GetValue(12)), Convert.ToDouble(reader.GetValue(13)), Convert.ToDouble(reader.GetValue(14)), Convert.ToDouble(reader.GetValue(15)), Convert.ToDouble(reader.GetValue(16)), Convert.ToDouble(reader.GetValue(17)), reader.GetString(18), Convert.ToDouble(reader.GetValue(19)), Convert.ToDouble(reader.GetValue(20)), Convert.ToDouble(reader.GetValue(21)), Convert.ToDouble(reader.GetValue(22)), Convert.ToDouble(reader.GetValue(23)), Convert.ToDouble(reader.GetValue(24)), Convert.ToDouble(reader.GetValue(25)), Convert.ToDouble(reader.GetValue(26)), Convert.ToDouble(reader.GetValue(27)), Convert.ToDouble(reader.GetValue(28)), Convert.ToDouble(reader.GetValue(29)), Convert.ToDouble(reader.GetValue(30)), Convert.ToDouble(reader.GetValue(31)), Convert.ToDouble(reader.GetValue(32)), Convert.ToDouble(reader.GetValue(33))));
             } while (reader.NextResult());
-
+            //var maximo = paisesViewModel.OrderByDescending(x => x.TotalCases).First();
             return View();
         }
 
@@ -49,7 +50,7 @@ namespace ProyectoFinal.Controllers
         public IEnumerable<PaisViewModel> GetDataByDate(string Date) => paisesViewModel.Where(x => x.Date == Date);
 
         [HttpGet]
-        public IEnumerable<PaisViewModel> GetDataByCountry(string Country) => paisesViewModel.Where(x => x.Location == Country);
+        public IEnumerable<PaisViewModel> GetDataByCountry(string Country) => paisesViewModel.OrderBy(x => x.Date).Where(x => x.Location == Country);
         
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
